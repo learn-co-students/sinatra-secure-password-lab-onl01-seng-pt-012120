02817c0b71525758fd6cceede26d28144f9bc35c
James Rogers <jarogers095@gmail.com>
2020-04-26 07:56:18 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  
+  def deposit(amount)
+    self.balance += amount
+    self.save
+  end
+  
+  def withdraw(amount)
+    if amount <= self.balance
+      self.balance -= amount
+      self.save
+    end
+  end
 end
